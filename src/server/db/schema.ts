3e4cd28aeedd56d13f6e2fa23994278bda7027e2
stautuan <stautuan89@gmail.com>
2024-06-23 14:12:13 -0400

    Uploadthing added

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -24,10 +24,12 @@ export const images = createTable(
     id: serial("id").primaryKey(),
     name: varchar("name", { length: 256 }).notNull(),
     url: varchar("url", { length: 1024 }).notNull(),
-    createdAt: timestamp("created_at")
+    userId: varchar("userId", { length: 256 }).notNull(),
+
+    createdAt: timestamp("created_at", { withTimezone: true })
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
-    updatedAt: timestamp("updatedAt"),
+    updatedAt: timestamp("updatedAt", { withTimezone: true }),
   },
   (example) => ({
     nameIndex: index("name_idx").on(example.name),
