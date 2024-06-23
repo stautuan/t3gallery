824582e4aeb28355ce8c82c9b2918f140ae9bcc8
stautuan <stautuan89@gmail.com>
2024-06-23 12:27:39 -0400

    force dynamic + vscode stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
-import Link from "next/link";
 import { db } from "~/server/db";
+
+export const dynamic = "force-dynamic";
 
 const mockUrls = [
   "https://utfs.io/f/e00a4be6-1b1e-4292-a6c0-546ccb8537ce-vmbc5d.jpg",
