ba4bd976afbabb4222d7144d305bc3f76417b5a4
stautuan <stautuan89@gmail.com>
2024-06-23 14:25:20 -0400

    Break queries into separate file

--- README.md
+++ README.md
@@ -8,8 +8,8 @@
 - [x] Actually set up a database (vercel postgres)
 - [x] Attach database to UI
 - [x] Add authentication (w/ clerk)
-- [ ] Add image upload
-- [ ] "taint" (server-only)
+- [x] Add image upload
+- [x] "taint" (server-only)
 - [ ] Use Next/Image component
 - [ ] Error management (w/ Sentry)
 - [ ] Routing/image page (parallel route)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
-import { db } from "~/server/db";
+
+import { getMyImages } from "~/server/queries";
 
 export const dynamic = "force-dynamic";
 
 async function Images() {
-  const images = await db.query.images.findMany({
-    orderBy: (model, { desc }) => desc(model.id),
-  });
+  const images = await getMyImages();
 
   return (
     <div className="flex flex-wrap gap-4">
@@ -19,6 +18,7 @@ async function Images() {
     </div>
   );
 }
+
 export default async function HomePage() {
   return (
     <main className="">
